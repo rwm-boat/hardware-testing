7f67a64c783a8f8cc027e7f45a40d30e7287b1e6
Daniel <danielshrives@gmail.com>
2019-08-15 22:52:28 -0400

    cap

--- LCD_test.py
+++ LCD_test.py
@@ -1,7 +1,7 @@
 from RPLCD import CharLCD
 from RPi import GPIO
 
-GPIO.setWarnings(False)
+GPIO.setwarnings(False)
 
 lcd = CharLCD(numbering_mode=GPIO.BOARD, cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[40, 38, 36, 32, 33, 31, 29, 23])
 lcd.write_string('Hello world!')
