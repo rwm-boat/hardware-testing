b8462956b17c498a25cdaa7884de7102723dd2db
Daniel <danielshrives@gmail.com>
2019-08-15 22:16:13 -0400

    installed RPi on pi

--- LCD_test.py
+++ LCD_test.py
@@ -1,6 +1,6 @@
 from RPLCD import CharLCD
 from RPi import GPIO
 
-lcd = CharLCD(numbering_mode=GPIO.BOARD, cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[40, 38, 36, 32, 33, 31, 29, 23])lcd.write(u'Hello world!')
+lcd = CharLCD(numbering_mode=GPIO.BOARD, cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[40, 38, 36, 32, 33, 31, 29, 23])
 
-lcd.write_string('hello')
+lcd.write(u'Hello world!')
