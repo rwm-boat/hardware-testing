5fa307005a4d5a1e158d5e0df8da835a7df130e4
Daniel <danielshrives@gmail.com>
2019-08-15 22:10:44 -0400

    updated imports again

--- LCD_test.py
+++ LCD_test.py
@@ -1,3 +1,6 @@
-from RPLCD.gpio import CharLCD
-lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[40, 38, 36, 32, 33, 31, 29, 23])
-lcd.write_string(u'Hello world!')
+from RPLCD import CharLCD
+from RPi import GPIO
+
+lcd = CharLCD(numbering_mode=GPIO.BOARD, cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[40, 38, 36, 32, 33, 31, 29, 23])lcd.write(u'Hello world!')
+
+lcd.write_string('hello')
