c26af2449e1a734b3c59cf722b6de81a5510a77c
Daniel <danielshrives@gmail.com>
2019-08-29 18:06:34 -0400

    servo degrees function

--- servo_sweep.py
+++ servo_sweep.py
@@ -13,21 +13,22 @@ bus.write_byte_data(addr, 0xfe, 0x79) #changes the prescale register to 50Hz
 bus.write_byte_data(addr, 0, 0x20) # enables the chip
 time.sleep(.25)
 
-# comment
-# Brent's comment
-
 bus.write_word_data(addr, 0x06, 0) # ch1 startaddress
 
-for x in range(125):
-    
-    bus.write_word_data(addr, 0x08, x*10) # ch1 end adress
-    time.sleep(.10)
-    bus.write_word_data(addr, 0x06, 0)
-    print("loop")
-for x in range(125):
+servo_to_degrees(0x06, 0x08, 25)
+time.sleep(.20)
 
-    bus.write_word_data(addr, 0x08, x*10)
-    time.sleep(.10)
-    bus.write_word_data(addr, 0x06, 0)
-    print("loop back")
+servo_to_degrees(0x06, 0x08, 45)
+time.sleep(.20)
 
+servo_to_degrees(0x06, 0x08, 90)
+time.sleep(.20)
+
+def servo_to_degrees(servo_start, servo_end, degrees):
+    ms_degree = 1250/360
+    servo_time = ms_degree * degrees
+
+    bus.write_word_data(addr, servo_end, servo_time) 
+    time.sleep(.20)
+    bus.write_word_data(addr, servo_start, 0)
+    print(degree + " : " + servo_time)
