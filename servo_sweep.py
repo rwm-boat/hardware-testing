820bab2d8d33430eacc99061a0ad5768ece27619
Daniel Shrives <danielshrives@gmail.com>
2019-08-30 23:44:12 +0100

    correct mircosecond range for the servo in. Servo will got to 0-180 position

--- servo_sweep.py
+++ servo_sweep.py
@@ -2,20 +2,24 @@ from adafruit_servokit import ServoKit
 import time
 kit = ServoKit(channels=16)
 
-kit.servo[0].actuation_range = 180
-kit.servo[0].set_pulse_width_range(500,2500)
+kit.servo[0].actuation_range = 180 #for DS3218mg servos
+kit.servo[0].set_pulse_width_range(500,2400) #correct microsecond range for DS3218mg servos
 
 
-kit.servo[0].angle = 0
-time.sleep(.25)
-kit.servo[0].angle = 90
-time.sleep(.25)
 kit.servo[0].angle = 180
-time.sleep(.25)
-kit.servo[0].angle = 90
-time.sleep(.25)
+time.sleep(5)
+
 kit.servo[0].angle = 0
-time.sleep(.25)
+time.sleep(5)
+
+#kit.servo[0].angle = 90
+#time.sleep(.25)
+#kit.servo[0].angle = 180
+#time.sleep(.25)
+#kit.servo[0].angle = 90
+#time.sleep(.25)
+#kit.servo[0].angle = 0
+#time.sleep(.25)
 
 # for x in range(180):
 #    kit.servo[0].angle = x
