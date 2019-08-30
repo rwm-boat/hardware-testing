54cdb2416070e9e77c2d8920b89c5138a732af7c
Daniel <danielshrives@gmail.com>
2019-08-30 16:04:44 -0400

    adafruit servokit

--- servo_sweep.py
+++ servo_sweep.py
@@ -1,37 +1,10 @@
-import smbus, time
+from adafruit_servokit import ServoKit
+import time
+kit = ServoKit(channels=16)
 
-bus = smbus.SMBus(1)
-addr = 0x40
+kit.servo[0].angle = 180
+time.sleep(1)
+kit.servo[0].angle = 0
 
-bus.write_byte_data(addr, 0, 0x20) # enable the chip
-time.sleep(.25)
-
-bus.write_byte_data(addr,0, 0x10) # enablpe prescale change as noted in datasheet
-time.sleep(.25)
-
-bus.write_byte_data(addr, 0xfe, 0x79) #changes the prescale register to 50Hz
-bus.write_byte_data(addr, 0, 0x20) # enables the chip
-time.sleep(.25)
-
-bus.write_word_data(addr, 0x06, 0) # ch1 startaddress
-
-def servo_to_degrees(servo_start, servo_end, degrees):
-    ms_degree = 1250/360
-    servo_time = ms_degree * degrees
-
-    bus.write_word_data(addr, int(servo_end), int(servo_time)) 
-    time.sleep(.20)
-    bus.write_word_data(addr, servo_start, 0)
-   # print(degrees + " : " + servo_time)
-
-servo_to_degrees(0x06, 0x08, 0)
-time.sleep(.50)
-
-servo_to_degrees(0x06, 0x08, 25)
-time.sleep(.50)
-
-servo_to_degrees(0x06, 0x08, 45)
-time.sleep(.50)
-
-servo_to_degrees(0x06, 0x08, 90)
-time.sleep(.50)
+# kit.servo[0].actuation_range = 180
+# kit.servo[0].set_pulse_width_range(1000,2000)
