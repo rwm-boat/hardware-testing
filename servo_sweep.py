85f3f29741ba17be09ca5b7e15df3eff8e9bc455
Daniel Shrives <danielshrives@gmail.com>
2019-08-31 00:21:50 +0100

    Calibrated ESC control freqs

--- servo_sweep.py
+++ servo_sweep.py
@@ -1,21 +1,24 @@
 from adafruit_servokit import ServoKit
 import time
 kit = ServoKit(channels=16)
+
 ESC = kit.servo[0]
 DIR = kit.servo[1]
 RB = kit.servo[2]
 
 # ESC
-ESC.actuation_range = 180 #for DS3218mg servos
-ESC.set_pulse_width_range(500,2400) #correct microsecond range for DS3218mg servos
+ESC.actuation_range = 180 
+ESC.set_pulse_width_range(930,2300) #correct microsecond range for Turnigy 70A ESC
 
 # DIR
 DIR.actuation_range = 180
-DIR.set_pulse_width_range(500, 2400)
+DIR.set_pulse_width_range(500, 2400) #correct microsecond range for DS3218mg servos
 
 # RB
 RB.actuation_range = 180
-RB.set_pulse_width_range(500, 2400)
+RB.set_pulse_width_range(500, 2400) #correct microsecond range for DS3218mg servos
+
+
 
 ESC.angle = 0
 DIR.angle = 0
@@ -23,26 +26,10 @@ RB.angle = 0
 
 time.sleep(3)
 
-ESC.angle = 180
 DIR.angle = 180
 RB.angle = 180
 
-time.sleep(3)
+ESC.angle = 10
+time.sleep(2)
+ESC.angle = 0
 
-
-#kit.servo[0].angle = 90
-#time.sleep(.25)
-#kit.servo[0].angle = 180
-#time.sleep(.25)
-#kit.servo[0].angle = 90
-#time.sleep(.25)
-#kit.servo[0].angle = 0
-#time.sleep(.25)
-
-# for x in range(180):
-#    kit.servo[0].angle = x
-#    time.sleep(.1)
-
-# for x in range(180,0):
-#    kit.servo[0].angle = x
-#    time.sleep(.1)
