606b6b757cb6368715cb3fb99aace092d5d6cec5
brentrolfes <brentrolfes20@gmail.com>
2019-08-30 18:51:20 -0400

    testing three servos

--- servo_sweep.py
+++ servo_sweep.py
@@ -1,16 +1,34 @@
 from adafruit_servokit import ServoKit
 import time
 kit = ServoKit(channels=16)
+ESC = kit.servo[0]
+DIR = kit.servo[1]
+RB = kit.servo[2]
 
-kit.servo[0].actuation_range = 180 #for DS3218mg servos
-kit.servo[0].set_pulse_width_range(500,2400) #correct microsecond range for DS3218mg servos
+# ESC
+ESC.actuation_range = 180 #for DS3218mg servos
+ESC.set_pulse_width_range(500,2400) #correct microsecond range for DS3218mg servos
 
+# DIR
+DIR.actuation_range = 180
+DIR.set_pulse_width_range(500, 2400)
 
-kit.servo[0].angle = 180
-time.sleep(5)
+# RB
+RB.actuation_range = 180
+RB.set_pulse_width_range(500, 2400)
 
-kit.servo[0].angle = 0
-time.sleep(5)
+ESC.angle = 0
+DIR.angle = 0
+RB.angle = 0
+
+time.sleep(3)
+
+ESC.angle = 180
+DIR.angle = 180
+RB.angle = 180
+
+time.sleep(3)
+
 
 #kit.servo[0].angle = 90
 #time.sleep(.25)
