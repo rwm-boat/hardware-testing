1bbfb442856b5b6d30a7683ff3860a141d221386
Daniel Shrives <danielshrives@gmail.com>
2019-08-31 02:00:37 +0100

    created small test

--- servo_sweep.py
+++ servo_sweep.py
@@ -6,6 +6,9 @@ ESC = kit.servo[0]
 DIR = kit.servo[1]
 RB = kit.servo[2]
 
+#------Tuining Variables
+reverse_limit = 0.3
+
 # ESC
 ESC.actuation_range = 180 
 ESC.set_pulse_width_range(930,2300) #correct microsecond range for Turnigy 70A ESC
@@ -19,48 +22,53 @@ RB.actuation_range = 180
 RB.set_pulse_width_range(500, 2400) #correct microsecond range for DS3218mg servos
 
 def setup():
-    ESC.angle = 0
-    DIR.angle = 90
-    RB.angle = 0
+    th_rq(0)
+    dir_rq(0)
+    rb_rq(1)
+    time.sleep(5)
+    print("ESC ARMED")
 
 
 def th_rq(mag):
 
     if mag > 100:
+        mag = 100
         print("Throttle limited to 100")
 
     if mag < -100:
+        mag = -100
         print("Throttle limited to -100")
 
     vel = ((abs(mag))*18)/10
-
-    if vel <= 10 and vel > 0:
-        vel = 10
-
-    if vel >= 180:
-        vel = 180
 
 #---------------reverse bucket
     if mag > 0:
         rb_rq(1)
 
     if mag < 0:
+        vel = vel*reverse_limit
         rb_rq(3)
+
 #------------------------------
-    
+    if vel < 10 and vel > 0:
+        vel = 10
+
+    if vel > 180:
+        vel = 180
+
     ESC.angle = vel
     print(vel)
 
 
 def rb_rq(level): #not set values
 
-    if level = 1: #up
+    if level == 1: #up
         RB.angle = 0
 
-    if level = 2: #mid
+    if level == 2: #mid
         RB.angle = 50
 
-    if level = 3: #down
+    if level == 3: #down
         RB.angle = 100
 
 
@@ -71,12 +79,18 @@ def dir_rq(angle): #range is -25 to 25 degrees
         print("Director limited to 25")
 
     if angle < -25:
-        angle = -25:
-            print("Director limited to -25")
+        angle = -25
+        print("Director limited to -25")
 
     servo_angle = 90 + angle
 
     DIR.angle = servo_angle
 
 
+setup()
+th_rq(30)
+time.sleep(2)
+th_rq(-30)
+time.sleep(3)
+th_rq(0)
 
