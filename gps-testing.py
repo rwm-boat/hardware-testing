eba7df0800f3d2be5bedc71173c218451e118f4d
brentrolfes <brentrolfes20@gmail.com>
2019-09-14 21:09:22 -0400

    dsad

--- gps-testing.py
+++ gps-testing.py
@@ -6,7 +6,7 @@ import datetime
 import os
 
 gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
-
+#another test
 # If the IMU is upside down (Skull logo facing up), change this value to 1
 IMU_UPSIDE_DOWN = 0
 
