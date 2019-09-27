71e4aaadd3b5385005b5b9cc1d5aa670b24a7621
brentrolfes <brentrolfes20@gmail.com>
2019-09-27 17:21:09 -0400

    made compass looking nicer

--- compass-testing.py
+++ compass-testing.py
@@ -27,7 +27,7 @@ while True:
     mag_x, mag_y, mag_z = sensor.magnetic
     gyro_x, gyro_y, gyro_z = sensor.gyro
     temp = sensor.temperature
-    compass = -(14 + numpy.degrees(numpy.arctan2(mag_x, mag_y)))
+    compass = round(-(14 + numpy.degrees(numpy.arctan2(mag_x, mag_y))))
     if compass < 0:
         compass = 360 + compass
 
@@ -38,4 +38,4 @@ while True:
    # print('Gyroscope (degrees/sec): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(gyro_x, gyro_y, gyro_z))
     #print('Temperature: {0:0.3f}C'.format(temp))
     # Delay for a second.
-    time.sleep(0.5)
+    time.sleep(0.25)
