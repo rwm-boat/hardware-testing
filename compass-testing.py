123eac0b86750f22b85e9ab81caf57d9072cfea1
brentrolfes <brentrolfes20@gmail.com>
2019-09-27 17:17:02 -0400

    dasd

--- compass-testing.py
+++ compass-testing.py
@@ -33,8 +33,8 @@ while True:
 
     # Print values.
    # print('Acceleration (m/s^2): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(accel_x, accel_y, accel_z))
-    print('Magnetometer (gauss): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(mag_x, mag_y, mag_z))
-    print(compass)
+    #print('Magnetometer (gauss): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(mag_x, mag_y, mag_z))
+    print("Heading: %s" %(compass))
    # print('Gyroscope (degrees/sec): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(gyro_x, gyro_y, gyro_z))
     print('Temperature: {0:0.3f}C'.format(temp))
     # Delay for a second.
