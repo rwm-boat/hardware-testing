fce9ab9183f15a197d0e50139277033b909b3480
danielshrives <danielshrives@gmail.com>
2019-09-28 16:28:30 -0400

    change

--- gps3-testing.py
+++ gps3-testing.py
@@ -11,20 +11,21 @@ pubber = Publisher(client_id="gps-values")
 
 while True:  # All data is available via instantiated thread data stream attribute.
     # line #140-ff of /usr/local/lib/python3.5/dist-packages/gps3/agps.py
-    print('---------------------')
-    print(                   agps_thread.data_stream.time)
-    print(agps_thread.data_stream.lat)
-    print('Lon:{}   '.format(agps_thread.data_stream.lon))
-    print('Speed:{} '.format(agps_thread.data_stream.speed))
-    print('Course:{}'.format(agps_thread.data_stream.track))
-    print('---------------------')
+    # print('---------------------')
+    # print(                   agps_thread.data_stream.time)
+    # print('Lat:{}   '.format(agps_thread.data_stream.lat))
+    # print('Lon:{}   '.format(agps_thread.data_stream.lon))
+    # print('Speed:{} '.format(agps_thread.data_stream.speed))
+    # print('Course:{}'.format(agps_thread.data_stream.track))
+    # print('---------------------')
 
-    # message = {
-    #     "time" :  agps_thread.data_stream.time,
-    #     "latitude" : agps_thread.data_stream.lat,
-    #     "longitude" : agps_thread.data_stream.lon
-    # }
+    message = {
+        'time' :  agps_thread.data_stream.time,
+        'latitude' : agps_thread.data_stream.lat,
+        'longitude' : agps_thread.data_stream.lon
+    }
 
-    # app_json = json.dump(message)
-    # print(app_json)
+    app_json = json.dump(message)
+    print(app_json)
+
     time.sleep(1) # Sleep, or do other things for as long as you like.
