957c316b0a1b8fe655d76ad9995b6bcad76ad150
danielshrives <danielshrives@gmail.com>
2019-09-30 01:20:05 -0400

    change

--- led-testing.py
+++ led-testing.py
@@ -1,17 +1,23 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(37,GPIO.OUT)
-GPIO.setup(35,GPIO.OUT)
-GPIO.setup(33,GPIO.OUT)
-print ("LED on")
-GPIO.output(37,GPIO.HIGH)
-GPIO.output(35,GPIO.HIGH)
-GPIO.output(33,GPIO.HIGH)
-time.sleep(3)
-print ("LED off")
-GPIO.output(37,GPIO.LOW)
-GPIO.output(35,GPIO.LOW)
-GPIO.output(33,GPIO.LOW)
+try:
+    while True:
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(26,GPIO.OUT)
+        GPIO.setup(19,GPIO.OUT)
+        GPIO.setup(13,GPIO.OUT)
+        print ("LED on")
+        GPIO.output(26,GPIO.HIGH)
+        GPIO.output(19,GPIO.HIGH)
+        GPIO.output(13,GPIO.HIGH)
+        time.sleep(1)
+        print ("LED off")
+        GPIO.output(26,GPIO.LOW)
+        GPIO.output(19,GPIO.LOW)
+        GPIO.output(13,GPIO.LOW)
+except KeyboardInterrupt:
+    print("ended")
+
+
