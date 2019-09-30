c2e67eb3912b966becddfcf7280830a3199de067
danielshrives <danielshrives@gmail.com>
2019-09-30 01:11:55 -0400

    led full test

--- led-testing.py
+++ led-testing.py
@@ -1,10 +1,18 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(17,GPIO.OUT)
-print ("LED on")
-GPIO.output(17,GPIO.HIGH)
-time.sleep(1)
-print ("LED off")
-GPIO.output(17,GPIO.LOW)
+
+while True:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(37,GPIO.OUT)
+    GPIO.setup(35,GPIO.OUT)
+    GPIO.setup(33,GPIO.OUT)
+    print ("LED on")
+    GPIO.output(37,GPIO.HIGH)
+    GPIO.output(35,GPIO.HIGH)
+    GPIO.output(33,GPIO.HIGH)
+    time.sleep(3)
+    print ("LED off")
+    GPIO.output(37,GPIO.LOW)
+    GPIO.output(35,GPIO.LOW)
+    GPIO.output(33,GPIO.LOW)
