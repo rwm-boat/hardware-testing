2908e21ae6be1ee22754fdbabee4ec18e7d6b5fb
Daniel <danielshrives@gmail.com>
2019-10-04 20:32:42 -0400

    un comment temp code

--- jet-testing.py
+++ jet-testing.py
@@ -8,13 +8,13 @@ import glob
 from mqtt_client.publisher import Publisher
 import json
 
-# Setup Temp Sensor
-# os.system('modprobe w1-gpio')
-# os.system('modprobe w1-therm')
+Setup Temp Sensor
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
  
-# base_dir = '/sys/bus/w1/devices/'
-# device_folder = glob.glob(base_dir + '28*')[0]
-# device_file = device_folder + w1_slave'
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + w1_slave'
 
 # Setup ADC Sensor
 i2c = busio.I2C(board.SCL, board.SDA)
--- telemetry.py
+++ telemetry.py
@@ -7,11 +7,13 @@ import json
 
 # global variables for UI
 mag_compass_reading = 0
+int_compass_reading = 0
 time_reading = 0
 lat_reading = 0
 lon_reading = 0
 speed_reading = 0
 gps_heading_reading = 0
+
 jet1_current = 0 #starboard
 jet2_current = 0 #port
 
@@ -20,6 +22,11 @@ def on_compass_received(client, userdata, message):
     global mag_compass_reading
     obj = json.loads(message.payload.decode('utf-8'))
     mag_compass_reading = obj['compass']
+
+def on_internal_compass_received(client, userdata, message):
+    global int_compass_reading
+    obj = json.loads(message.payload.decode('utf-8'))
+    int_compass_reading = obj['heading']
 
 def on_gps_received(client, userdata, message):
     # create global variables for UI
@@ -37,6 +44,7 @@ def on_gps_received(client, userdata, message):
     lon_reading = obj['longitude']
     speed_reading = obj["speed"]
     gps_heading_reading = obj["course"]
+
 def on_adc_received(client, userdata, message):
     global jet1_current
     global jet2_current
@@ -44,7 +52,6 @@ def on_adc_received(client, userdata, message):
     obj = json.loads(message.payload.decode('utf-8'))
     jet1_current = obj["jet1_amps"]
     jet2_current = obj["jet2_amps"]
-
 
 def draw(stdscr):
     # Make stdscr.getch non-blocking
@@ -85,46 +92,49 @@ def draw(stdscr):
         stdscr.addstr(2,0,"Mag Compass: ")
         stdscr.addstr(2,second_column_width,str(mag_compass_reading))
 
-        stdscr.addstr(3,0,"GPS Compass: ")
-        stdscr.addstr(3,second_column_width,str(gps_heading_reading))
+        stdscr.addstr(3,0,"Int. Mag Compass: ")
+        stdscr.addstr(3,second_column_width,str(round(int_compass_reading,2)))
 
-        stdscr.addstr(4,0,"GPS Time: ")
-        stdscr.addstr(4,second_column_width,str(time_reading))
+        stdscr.addstr(4,0,"GPS Compass: ")
+        stdscr.addstr(4,second_column_width,str(gps_heading_reading))
 
-        stdscr.addstr(5,0,"Latitude: ")
-        stdscr.addstr(5,second_column_width,str(lat_reading))
+        stdscr.addstr(5,0,"GPS Time: ")
+        stdscr.addstr(5,second_column_width,str(time_reading))
 
-        stdscr.addstr(6,0,"Longitude: ")
-        stdscr.addstr(6,second_column_width,str(lon_reading))
+        stdscr.addstr(6,0,"Latitude: ")
+        stdscr.addstr(6,second_column_width,str(round(lat_reading,6)))
 
-        stdscr.addstr(7,0,"GPS Speed: ")
-        stdscr.addstr(7, second_column_width, str(speed_reading))
+        stdscr.addstr(7,0,"Longitude: ")
+        stdscr.addstr(7,second_column_width,str(round(lon_reading,6)))
+
+        stdscr.addstr(8,0,"GPS Speed(kn): ")
+        stdscr.addstr(8, second_column_width, str(round(speed_reading,2)))
 
         # --- JET VALUES ---
 
-        stdscr.addstr(8,start_x_partition, "--------------- JET ---------------")
-        stdscr.addstr(9,0,"Starboard Jet Current : ")
+        stdscr.addstr(9,start_x_partition, "--------------- JET ---------------")
+        stdscr.addstr(10,0,"Starboard Jet Current : ")
         
         if(jet1_current < 0):
-            stdscr.addstr(9,second_column_width,str(jet1_current), curses.color_pair(1))
+            stdscr.addstr(10,second_column_width,str(round(jet1_current,2)), curses.color_pair(1))
         else:
-            stdscr.addstr(9,second_column_width,str(jet1_current), curses.color_pair(2))
+            stdscr.addstr(10,second_column_width,str(round(jet1_current,2)), curses.color_pair(2))
 
-        stdscr.addstr(10,0,"Port Jet Current: ")
+        stdscr.addstr(11,0,"Port Jet Current: ")
 
         if(jet2_current < 0):
-            stdscr.addstr(10,second_column_width,str(jet2_current), curses.color_pair(1))
+            stdscr.addstr(11,second_column_width,str(round(jet2_current,2)), curses.color_pair(1))
         else:
-            stdscr.addstr(10,second_column_width,str(jet2_current), curses.color_pair(2))
+            stdscr.addstr(11,second_column_width,str(round(jet2_current,2)), curses.color_pair(2))
 
-        stdscr.addstr(11,0,"JET Current Delta: ")
+        stdscr.addstr(12,0,"JET Current Delta: ")
 
         jet_delta = jet1_current - jet2_current
 
         if(jet_delta < -3 or jet_delta > 3):
-            stdscr.addstr(11,second_column_width,str(jet_delta), curses.color_pair(1))
+            stdscr.addstr(12,second_column_width,str(round(jet_delta,2)), curses.color_pair(1))
         else:
-            stdscr.addstr(11,second_column_width,str(jet_delta), curses.color_pair(2))
+            stdscr.addstr(12,second_column_width,str(round(jet_delta,2)), curses.color_pair(2))
 
 
         
@@ -143,7 +153,8 @@ if __name__ == '__main__':
         default_subscriptions = {
             "/status/compass": on_compass_received,
             "/status/gps" : on_gps_received,
-            "/status/adc" : on_adc_received
+            "/status/adc" : on_adc_received,
+            "/status/internal_compass" : on_internal_compass_received
         }
         subber = Subscriber(client_id="telemetry_live", broker_ip="192.168.1.170", default_subscriptions=default_subscriptions)
         thread = Thread(target=subber.listen)
