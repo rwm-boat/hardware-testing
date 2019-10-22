ea849ca1dac5d5fd5a6cd4f00cbcf20de5afa5a0
Daniel <danielshrives@gmail.com>
2019-10-22 16:50:14 -0400

    servo_sweep encorporates both jets

--- examples/servo_sweep.py
+++ examples/servo_sweep.py
@@ -2,34 +2,53 @@ from adafruit_servokit import ServoKit
 import time
 kit = ServoKit(channels=16)
 
-ESC = kit.servo[0]
-DIR = kit.servo[1]
-RB = kit.servo[2]
+#Port Jet (Red Wires to Pi) JET2
+PESC = kit.servo[0]
+PDIR = kit.servo[1]
+PRB = kit.servo[2]
+ 
+#Starboard Jet (Black Wires to Pi) JET1
+SESC = kit.servo[4]
+SDIR = kit.servo[5]
+SRB = kit.servo[6]
 
 #------Tuining Variables
 reverse_limit = 0.3
 
 # ESC
-ESC.actuation_range = 180 
-ESC.set_pulse_width_range(930,2300) #correct microsecond range for Turnigy 70A ESC
+PESC.actuation_range = 180 
+PESC.set_pulse_width_range(930,2300) #correct microsecond range for Turnigy 70A ESC
+
+SESC.actuation_range = 180 
+SESC.set_pulse_width_range(930,2300) #correct microsecond range for Turnigy 70A ESC
+
 
 # DIR
-DIR.actuation_range = 180
-DIR.set_pulse_width_range(500, 2400) #correct microsecond range for DS3218mg servos
+PDIR.actuation_range = 180
+PDIR.set_pulse_width_range(500, 2400) #correct microsecond range for DS3218mg servos
+
+SDIR.actuation_range = 180
+SDIR.set_pulse_width_range(500, 2400) #correct microsecond range for DS3218mg servos
 
 # RB
-RB.actuation_range = 180
-RB.set_pulse_width_range(500, 2400) #correct microsecond range for DS3218mg servos
+PRB.actuation_range = 180
+PRB.set_pulse_width_range(500, 2400) #correct microsecond range for DS3218mg servos
+
+SRB.actuation_range = 180
+SRB.set_pulse_width_range(500, 2400) #correct microsecond range for DS3218mg servos
 
 def setup():
-    th_rq(0)
-    dir_rq(0)
-    rb_rq(1)
+    th_rq(1,0)
+    dir_rq(1,0)
+    rb_rq(1,1)
+    th_rq(2,0)
+    dir_rq(2,0)
+    rb_rq(2,1)
     time.sleep(5)
     print("ESC ARMED")
 
 
-def th_rq(mag):
+def th_rq(jet,mag):
 
     if mag > 100:
         mag = 100
@@ -42,12 +61,12 @@ def th_rq(mag):
     vel = ((abs(mag))*18)/10
 
 #---------------reverse bucket
-    if mag > 0:
-        rb_rq(1)
+    # if mag > 0:
+    #     rb_rq(1)
 
-    if mag < 0:
-        vel = vel*reverse_limit
-        rb_rq(3)
+    # if mag < 0:
+    #     vel = vel*reverse_limit
+    #     rb_rq(3)
 
 #------------------------------
     if vel < 10 and vel > 0:
@@ -56,23 +75,36 @@ def th_rq(mag):
     if vel > 180:
         vel = 180
 
-    ESC.angle = vel
+    if jet == 1:
+        SESC.angle = vel
+    
+    if jet == 2:
+        PESC.angle = vel
+
+    #ESC.angle = vel
     print(vel)
 
 
-def rb_rq(level): #not set values
+def rb_rq(jet,level): #not set values
 
     if level == 1: #up
-        RB.angle = 0
+        pos = 0
 
     if level == 2: #mid
-        RB.angle = 50
+        pos = 50
 
     if level == 3: #down
-        RB.angle = 100
+        pos = 100
+
+    if jet == 1:
+        SRB.angle = pos
+    
+    if jet == 2:
+        PRB.angle = pos
 
 
-def dir_rq(angle): #range is -25 to 25 degrees
+
+def dir_rq(jet,angle): #range is -25 to 25 degrees
     
     if angle > 25:
         angle = 25
@@ -84,12 +116,21 @@ def dir_rq(angle): #range is -25 to 25 degrees
 
     servo_angle = 90 + angle
 
-    DIR.angle = servo_angle
+    if jet == 1:
+        SDIR.angle = servo_angle
+    
+    if jet == 2:
+        PDIR.angle = servo_angle
+
+    #DIR.angle = servo_angle
 
 
 setup()
-th_rq(10)
-rb_rq(2)
-dir_rq(20)
+th_rq(1,10)
+rb_rq(1,2)
+dir_rq(1,20)
+th_rq(2,10)
+rb_rq(2,2)
+dir_rq(2,20)
 
 
