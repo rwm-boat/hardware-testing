0c15b3dc45b806ec569ed8982b18530c9aa02ccd
danielshrives <danielshrives@gmail.com>
2019-11-09 13:46:19 -0500

    kalman filter added and functional

--- compass_filtering.py
+++ compass_filtering.py
@@ -5,12 +5,14 @@ import os
 import math
 from scipy.signal import butter, lfilter, freqz
 from scipy import signal
+import filterpy.kalman as kf
+from filterpy.stats import gaussian
 
 gps_speed = []
 gps_course = []
 mag_compass = []
 
-PATH = "/home/actual_daniel/125deg305_2019-11-7-15_35_22.txt"
+PATH = "/home/actual_daniel/Documents/Logs/11-8-19/Waypoint1Sec_2019-11-8-21:20:50.txt"
 
 # load log data into arrays
 def load_log():
@@ -20,7 +22,6 @@ def load_log():
     global mag_compass
 
     with open(PATH, "r") as log_file:
-        error_filter = 20
         for line in log_file.readlines():
             obj = json.loads(line)
             
@@ -46,6 +47,49 @@ def low_pass_filter(data, cutoff_freq, sample_freq, order=5):
     
     b, a = butter(order, w, 'low')
     output = signal.filtfilt(b, a, data)
+
+    return output
+
+def kalman_filter(data):
+    
+    output = []
+
+    ### sqrt of the variance is the error distance (aka. meters or degrees) ###
+    # how much error there is in the process model (how much do we trust the model)
+    process_var = .1
+    # how much error there is in each sensor measurement (how much do we trust the sensor)
+    sensor_var = 5
+
+    ## Initial State ##
+    # [position, variance, sensor_var] sensor state
+    # [(meters, degrees), (meters, degrees), ] ## remember sqrt of variance is distance error
+    x = gaussian(data[1], 20**2., sensor_var) 
+
+    #[position, velocity, process_var] of model initial state
+    # how we think the system works
+    process_model = gaussian(0., 0., process_var)
+
+    for z in data:
+        # -------- PREDICT --------- #
+        # X is the state of the system
+        # P is the variance of the system
+        # u is the movement of the system due to the process
+        # Q is the noise of the process
+        x, P = kf.predict(x=x, P=process_model)
+        print(x, process_model)
+        # sensor says z with a standard deviation of sensorvar**2
+        # probability of the measurement given the current state 
+        #likelihood = gaussian(z, mean(z), sensor_var) 
+
+        # -------- UPDATE --------- #
+        # X is the state of the system
+        # P is the variance of the system
+        # z is the measurement
+        # R is the measurement variance
+        x, P = kf.update(x=x, P=P, z=z, R=sensor_var)
+        #print( "x: ",'%.3f' % x, "var: " '%.3f' % P, "z: ", '%.3f' % z)
+        #print(x)
+        output.append(x.mean())
     return output
 
 #Plot of compass heading and speed
@@ -54,19 +98,17 @@ def generate_plot():
     fig, ax1 = plt.subplots()
 
     ax1.plot(mag_compass, label="Magnometer Heading", color = 'g')
-    ax1.plot(gps_course, label="GPS Heading", color = 'R')
+    # ax1.plot(gps_course, label="GPS Heading", color = 'r')
     
-    ax1.plot(moving_avg_filter(mag_compass, 10), label="Moving Average Mag Compass: " + str(10), color = 'k')
-    ax1.plot(low_pass_filter(mag_compass, .5, 10), label="lowpassfilter", color = 'c')
-        
+    ax1.plot(moving_avg_filter(mag_compass, 10), label="Moving Average Mag Compass: " + str(10), color = 'c')
+    ax1.plot(low_pass_filter(mag_compass, 1, 10), label="lowpassfilter", color = 'r')
+    ax1.plot(kalman_filter(mag_compass), label="kalman filter", color = 'k')
+    ax1.plot(low_pass_filter(kalman_filter(mag_compass),.5,10), label="kalman + low pass filter", color = 'b')
+
     ax1.legend(loc = 'lower right')
    
     ax2 = ax1.twinx()
     ax3 = ax1.twinx()
-    
-
-    # ax2.plot(gps_speed, label="GPS Speed", linestyle='--')
-    # ax2.legend(loc = 'upper right')
 
     plt.title("Boat Speed and Course " + os.path.basename(PATH))
     ax1.set_xlabel("Time (1/10 sec)")
@@ -78,4 +120,3 @@ def generate_plot():
 
 load_log()
 generate_plot()
-
