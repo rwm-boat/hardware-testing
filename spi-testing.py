520566197956f5870c9d23633dc3ff4577bec7f3
brentrolfes <brentrolfes20@gmail.com>
2020-03-20 19:34:06 -0400

    fixed

--- spi-testing.py
+++ spi-testing.py
@@ -10,24 +10,24 @@ def calcEvenParity(value):
     i = 0b0 
     
     for i in range(10):
-        if(value & 0x1)
+        if value & 0x1:
             cnt = cnt + 1
         value >>= 1
     return cnt & 0x1
 
 def read_rawAngle():
 
-# Raw Angle Adress 0x3FFF
-# Read Frame: (Parity(MSB),EF(Error Flag), 14 bit adressed data)
+    # Raw Angle Adress 0x3FFF
+    # Read Frame: (Parity(MSB),EF(Error Flag), 14 bit adressed data)
 
-angle_adress = 0x3FFF
-command = 0b0100000000000000
-command = command | registerAdress
+    angle_adress = 0x3FFF
+    command = 0b0100000000000000
+    command = command | registerAdress
 
-command |= (calcEvenParity(command)<<15)
+    command |= (calcEvenParity(command)<<15)
 
-reply = spi.xfer2(msg)
+    reply = spi.xfer2(msg)
 
-print(reply)
+    print(reply)
 
 
