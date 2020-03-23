2891e22d9f316f6160d1c5b267ca640e452fb856
brentrolfes <brentrolfes20@gmail.com>
2020-03-23 16:47:26 -0400

    a

--- spi-testing.py
+++ spi-testing.py
@@ -5,11 +5,11 @@ spi = spidev.SpiDev()
 spi.open(0, 0) # (bus, device)
 spi.max_speed_hz = 1000000 # 1MHz clock (AMS accepts up to 10MHz)
 
-    msg = [0b11111111, 0x00]
-    reply = spi.xfer2(msg)
+msg = [0b11111111, 0x00]
+reply = spi.xfer2(msg)
 
-    print("first frame: " + bin(reply[0])
-    print("second frame: " + bin(reply[1]))
+print("first frame: " + bin(reply[0])
+print("second frame: " + bin(reply[1]))
        
 for x in range(10):
     read_rawAngle()
