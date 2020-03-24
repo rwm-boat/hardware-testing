35ba0e544ee3c6efdab9ecccba95986f16ccd694
danielshrives <danielshrives@gmail.com>
2020-03-24 14:56:16 -0400

    a

--- spi-testing.py
+++ spi-testing.py
@@ -7,20 +7,17 @@ spi.max_speed_hz = 1000000 # 1MHz clock (AMS accepts up to 10MHz)
 spi.mode = 0b1
 spi.lsbfirst = False
        
-for x in range(10):
+for x in range(1000):
     msg = [0b11111111, 0b11111111]
     reply = spi.xfer2(msg)
-
-    print("first frame: " + bin(reply[0]))
-    print("second frame: " + bin(reply[1]))
 
     left_byte = reply[0]
     right_byte = reply[1]
 
-    rotation = (((left_byte & 0xFF) << 8) | ( right_byte & 0xFF)) & ~0xC000
+    raw_rotation = (((left_byte & 0xFF) << 8) | ( right_byte & 0xFF)) & ~0xC000
+    adj_rotation = raw_rotation / (0x3FFF/360)
 
-    print(rotation)
-    print(bin(rotation))
+    print(adj_rotation)
     
-    time.sleep(1)
+    time.sleep(0.1)
 
