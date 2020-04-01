a4ee7df23abac67b1ae17262aa770fee14036a56
daniel shrives <danielshrives@gmail.com>
2020-04-01 05:33:44 +0100

    travel testing

--- travel-testing.py
+++ travel-testing.py
@@ -1,21 +1,19 @@
-
-
-
 import RPi.GPIO as GPIO
-import numpy
-import time
-import json
 #from mqtt_client.publisher import Publisher
 #from mqtt_client.subscriber import Subscriber
 #from threading import Thread
 def on_count_up_command():
-    
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(26,GPIO.IN)
+    i = GPIO.input(26)
+    count = 0
     try:
         GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
-        resistor_val = GPIO.setup(26,GPIO.IN)
-        print(resistor_val)
-    except Exception:
-        print("LED FAILTURE")
+        GPIO.setup(26,GPIO.IN)
+        i = GPIO.input(26)
+        print(count)
+        print("I:" + i)
+    except:
+        print("hello")
 
 on_count_up_command()
