5be2489e2c5d905d996e9a2855c5cb6bbcd4c5a0
danielshrives <danielshrives@gmail.com>
2020-04-01 00:35:49 -0400

    travel testing

--- travel-testing.py
+++ travel-testing.py
@@ -1,4 +1,10 @@
+
+
+
 import RPi.GPIO as GPIO
+import numpy
+import time
+import json
 #from mqtt_client.publisher import Publisher
 #from mqtt_client.subscriber import Subscriber
 #from threading import Thread
@@ -8,12 +14,20 @@ def on_count_up_command():
     i = GPIO.input(26)
     count = 0
     try:
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(26,GPIO.IN)
-        i = GPIO.input(26)
-        print(count)
-        print("I:" + i)
-    except:
-        print("hello")
+        while (1):
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(26,GPIO.IN)
+            i = GPIO.input(26)
+            print(i)
+            if (i):
+                print("i = 1")
+            else:
+                while (not i):
+                    count = count
+                count = count + 1
+            print (count)
+            
+    except Exception:
+        print("Light Resistor FAILURE")
 
 on_count_up_command()
